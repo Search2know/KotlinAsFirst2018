35a485aacda68e75960943f2ecdc9b801aa0998d
Search2know <43117355+Search2know@users.noreply.github.com>
2018-10-24 22:10:50 +0300

    Add files via upload

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -22,10 +22,10 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
 fun isNumberHappy(number: Int): Boolean {
-    var a = number / 1000
-    var c = number / 100 % 10
-    var f = number / 10 % 10
-    var g = number % 10
+    val a = number / 1000
+    val c = number / 100 % 10
+    val f = number / 10 % 10
+    val g = number % 10
     return ((a + c) == (f + g))
 
 }
@@ -38,9 +38,9 @@ fun isNumberHappy(number: Int): Boolean {
  * Считать, что ферзи не могут загораживать друг друга.
  */
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    if ((x1 == x2) || (y1 == y2) || (abs(x1 - x2)) == (abs(y1 - y2)))
-        return true
-    else return false
+    return if ((x1 == x2) || (y1 == y2) || (abs(x1 - x2)) == (abs(y1 - y2)))
+        true
+    else false
 }
 
 
@@ -52,7 +52,7 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  */
 fun daysInMonth(month: Int, year: Int): Int = when {
     (month == 4) || (month == 6) || (month == 9) || (month == 11) -> 30
-    (month == 2) && (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) -> 29
+    (month == 2) && (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) -> 29
     (month == 2) -> 28
     else -> 31
 }
@@ -66,9 +66,9 @@ fun daysInMonth(month: Int, year: Int): Int = when {
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
                  x2: Double, y2: Double, r2: Double): Boolean {
-    var f = sqrt(sqr(y2 - y1) + sqr(x2 - x1) )
-    if ( r2 >= f+r1) return true
-    else return false
+    var f = sqrt(sqr(y2 - y1) + sqr(x2 - x1))
+    return if (r2 >= f + r1) true
+    else false
 }
 
 /**
